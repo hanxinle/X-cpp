9bc30946d7e665a9ae6739143d4e84bde1ff60d7
hanxinle <hanxinle199@gmail.com>
2021-05-13 22:11:24 +0800

    创建C++新特性学习仓库，写几行chrono相关代码

---
+++ src/cpp11to20/00-time/duration.cpp
@@ -0,0 +1,28 @@
+﻿/**
+* doc link:https://en.cppreference.com/w/cpp/header/chrono
+*
+*/
+#include<chrono>
+#include <iostream>
+using namespace std;
+
+int main() {
+    chrono::milliseconds ms(3);
+    chrono::microseconds us = 2*ms;
+
+    cout << ms.count() << endl;
+    cout << us.count() << endl;
+
+    chrono::minutes t1(10);  //                                                                                                        10 min
+    chrono::seconds t2(60);  // 60 s
+    auto t3 = t1- t2;
+    cout << t3.count() << endl;
+    cout << typeid(t3).name() << endl;
+    cout << t3.count() << endl;
+
+    chrono::seconds t4 = t1 - t2;
+    cout << typeid(t4).name() << endl;
+    cout << t4.count() << endl;
+
+    return 0;
+}
